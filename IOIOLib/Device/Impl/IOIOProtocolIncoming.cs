398587e9a3352609e9478e1858d494c838b1b795
Joe Freeman <info@freemansoft.com>
2015-03-08 07:25:15 -0400

    Add BSD license

--- IOIOLib/Device/Impl/IOIOProtocolIncoming.cs
+++ IOIOLib/Device/Impl/IOIOProtocolIncoming.cs
@@ -1,4 +1,33 @@
-﻿using IOIOLib.Device.Types;
+﻿/*
+ * Copyright 2011 Ytai Ben-Tsvi. All rights reserved.
+ * Copyright 2015 Joe Freeman. All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ * 
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED 'AS IS AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARSHAN POURSOHI OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied.
+ */
+
+using IOIOLib.Device.Types;
 using IOIOLib.IOIOException;
 using IOIOLib.Util;
 using System;
@@ -87,23 +116,23 @@ namespace IOIOLib.Device.Impl
                             throw new IOException("Unexpected stream_ closure");
                         }
 
-                        LOG.Debug(IncomingTask_.Id+" received 0x" + b.ToString("X"));
+                        LOG.Debug(IncomingTask_.Id + " received 0x" + b.ToString("X"));
                         return b;
                     }
                     catch (TimeoutException e)
                     {
-                        LOG.Debug(IncomingTask_.Id+" readByte " + e.Message + " retrying");
+                        LOG.Debug(IncomingTask_.Id + " readByte " + e.Message + " retrying");
                     }
                 }
             }
             catch (System.Threading.ThreadAbortException e)
             {
-                LOG.Warn(IncomingTask_.Id+" Thread aborted while in read ", e);
+                LOG.Warn(IncomingTask_.Id + " Thread aborted while in read ", e);
                 throw e;
             }
             catch (IOException e)
             {
-                LOG.Warn(IncomingTask_.Id+" IOIO disconnected while in read");
+                LOG.Warn(IncomingTask_.Id + " IOIO disconnected while in read");
                 throw e;
             }
         }
@@ -131,7 +160,7 @@ namespace IOIOLib.Device.Impl
                 {
                     cancelTokenSource_.Token.ThrowIfCancellationRequested();
                     arg1 = readByte();
-                    LOG.Debug(IncomingTask_.Id+" Processing reply type " + arg1.ToString("X"));
+                    LOG.Debug(IncomingTask_.Id + " Processing reply type " + arg1.ToString("X"));
                     switch (arg1)
                     {
                         case (int)IOIOProtocolCommands.ESTABLISH_CONNECTION:
@@ -390,16 +419,16 @@ namespace IOIOLib.Device.Impl
             }
             catch (System.Threading.ThreadAbortException e)
             {
-                LOG.Error(IncomingTask_.Id + " Probably aborted thread (TAE): ", e);
+                LOG.Error(IncomingTask_.Id + " Probably aborted thread (TAE): " + e.Message);
             }
             catch (ObjectDisposedException e)
             {
                 //// see this when steram is closed
-                LOG.Error(IncomingTask_.Id + " Probably closed incoming stream: (ODE)", e);
+                LOG.Error(IncomingTask_.Id + " Probably closed incoming stream: (ODE)" + e.Message);
             }
             catch (Exception e)
             {
-                LOG.Error(IncomingTask_.Id + " Probably stopping incoming: (E)", e);
+                LOG.Error(IncomingTask_.Id + " Probably stopping incoming: (E)" + e.Message);
             }
             finally
             {
