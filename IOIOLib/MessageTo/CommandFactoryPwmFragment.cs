0c3a0332f7ed759fc487caf8ecfe32ac037f21bb
Joe Freeman <info@freemansoft.com>
2015-03-20 23:04:06 -0400

    Revert "add missing pwm messageTo factory methods"
    This reverts commit a948c05543770d5b9fa773bca1badd809ba11602.

--- IOIOLib/MessageTo/CommandFactoryPwmFragment.cs
+++ IOIOLib/MessageTo/CommandFactoryPwmFragment.cs
@@ -11,33 +11,19 @@ namespace IOIOLib.MessageTo
 	 public partial class IOIOMessageCommandFactory
 	{
 
-		public IPwmOutputConfigureCommand CreateConfigurePwmOutput( DigitalOutputSpec spec, bool enable)
+		public IPwmOutputConfigureCommand CreateOpenPwmOutput( DigitalOutputSpec spec, bool enable)
 		{
 			return new PwmOutputConfigureCommand(spec, enable);
 		}
 
-		public IPwmOutputConfigureCommand CreateConfigurePwmOutput(DigitalOutputSpec spec, int freqHz)
+		public IPwmOutputConfigureCommand CreateOpenPwmOutput(DigitalOutputSpec spec, int freqHz)
 		{
 			return new PwmOutputConfigureCommand(spec, freqHz);
 		}
 
-		public IPwmOutputConfigureCommand CreateConfigurePwmOutput(DigitalOutputSpec spec, int freqHz, float dutyCycle)
+		public IPwmOutputConfigureCommand CreateOpenPwmOutput(DigitalOutputSpec spec, int freqHz, float dutyCycle)
 		{
 			return new PwmOutputConfigureCommand(spec, freqHz, dutyCycle);
-		}
-
-		public IPwmOutputUpdateCommand CreateUpdatePwmOutput(PwmOutputSpec spec, int freqHz)
-		{
-			return new PwmOutputUpdateCommand(spec, freqHz);
-		}
-		public IPwmOutputUpdateCommand CreateUpdatePwmOutput(PwmOutputSpec spec, int freqHz, float dutyCycle)
-		{
-			return new PwmOutputUpdateCommand(spec, freqHz, dutyCycle);
-		}
-
-		public IPwmOutputUpdateCommand CreateUpdatePwmOutput(PwmOutputSpec spec, float dutyCycle)
-		{
-			return new PwmOutputUpdateCommand(spec, dutyCycle);
 		}
 
 		public IPwmOutputCloseCommand CreateClosePwmOutput(PwmOutputSpec spec)
